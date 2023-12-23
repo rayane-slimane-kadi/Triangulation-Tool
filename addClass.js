5705bda398ca0b889a91dbb57c5b2d5723b2d2a4
Rayane Slimane-Kadi <rayane.slimane.k@gmail.com>
2023-12-23 11:50:43 -0500

    Cleaned up Classes Page

--- addClass.js
+++ addClass.js
@@ -1,6 +1,8 @@
 function createClassroom() {
+    event.preventDefault(); // Prevent the browser from refreshing when pressing the "Add" button
+
     //get classroom name from user
-    let classRoomInput = prompt("Please enter the name of the class", "Harry Potter");
+    let classRoomInput = prompt("Please enter the name of the class", "");
     
     //make button 60
     const newButton = document.createElement('button');
